9225d0866fd840b01a419069f2277474d02cfc06
tuba-gibi <eetede84@gmail.com>
2023-06-17 15:25:04 +0300

    api

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const btn = document.getElementById("btn");
+window.addEventListener = ("load", () =>{
+    getData();
+})
+// window.onload = func(){
+//     getData()
+// }
 
-btn.addEventListener("click", () => {
-
+const getData = async() =>{
+    const URL = "https://restcountries.com/v3.1/all?fields=name,flags`"
+    fetch('https://api.github.com/users/jessica-murphy/repos')
+  .then(res => res.json())
 }
