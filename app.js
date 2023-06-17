ba5ae0739a186db725d4f1c893e1379d69ea0b2a
tuba-gibi <eetede84@gmail.com>
2023-06-17 16:06:17 +0300

    js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-window.addEventListener = ("load", () =>{
+window.addEventListener =
+  ("load",() => {
     getData();
-})
+  });
 // window.onload = func(){
 //     getData()
 // }
 
-const getData = async() =>{
-    const URL = "https://restcountries.com/v3.1/all?fields=name,flags`"
-    fetch('https://api.github.com/users/jessica-murphy/repos')
-  .then(res => res.json())
-}
+const getData = async () => {
+  const URL ="https://restcountries.com/v3.1/all";
+  // const API_KEY = ""
+  const response = await fetch(URL);
+  console.log(response);
+//   if (!response.ok) {
+//     throw Error(response.statusText);
+//   }
+};
